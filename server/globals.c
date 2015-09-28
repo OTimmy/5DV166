c338e03228c09edc166a53d96155bd85e608f7f7
Joakim Sandman <tm08jsn@cs.umu.se>
2015-09-28 17:06:06 +0200

    Here are some globals

---
+++ server/globals.c
@@ -0,0 +1,50 @@
+/*
+ * globals.c
+ * Written by Joakim Sandman, September 2015.
+ * Last update: 27/9-15.
+ * Lab 1: Chattserver, Datakommunikation och datornät HT15.
+ *
+ * globals.c contains global variables and functions for using them.
+ */
+
+/* --- Standard headers --- */
+//#include <stdlib.h>
+//#include <stdio.h>
+//#include <time.h> /* -lrt (sometimes for glibc < 2.17) */
+//#include <math.h> /* -lm */
+//#include <string.h>
+//#include <sys/wait.h>
+/* --- Data types --- */
+//#include <stdbool.h>
+//#include <stdint.h> /* Subset of inttypes.h */
+#include <inttypes.h> /* Fixed width integers */
+//#include <sys/types.h>
+//#include <limits.h>
+//#include <stddef.h>
+//#include <ctype.h> /* E.g. isalnum(), tolower() */
+/* --- System calls --- */
+//#include <unistd.h>
+//#include <errno.h>
+//#include <dirent.h>
+//#include <sys/param.h> /* E.g. MAXPATHLEN for getcwd() */
+//#include <fcntl.h> /* File control (including sockets) */
+//#include <sys/stat.h> /* Stat function */
+//#include <sys/select.h>
+/* --- Signals and threads --- */
+//#include <signal.h>
+//#include <setjmp.h>
+//#include <pthread.h> /* -pthread &or -lpthread */
+/* --- Sockets --- */
+//#include <sys/socket.h>
+//#include <netinet/in.h>
+//#include <arpa/inet.h>
+//#include <endian.h>
+//#include <netdb.h>
+/* --- Functions --- */
+//#include <stdarg.h>
+
+/* --- Local headers --- */
+#include "globals.h"
+
+uint8_t nrof_clients = 0;
+
